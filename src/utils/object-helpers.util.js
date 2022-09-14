478632b8601a1cdb690a159d411aafcc428e8e77
thisisabhinay <a8h1nay@gmail.com>
2022-09-14 17:25:08 +0530

    Adds object helpers

---
+++ src/utils/object-helpers.util.js
@@ -0,0 +1,27 @@
+/**
+ * Recursively traversing the path of object properties
+ * as specified in props argument to get the inner most value
+ */
+const getNestedProp = (obj, props) => {
+    let index = 0
+    let currentValue = obj[props[index]]
+    if (props.length > 1 && typeof currentValue === "object")
+        return getNestedProp(currentValue, props.slice(index + 1))
+
+    return currentValue
+}
+
+const setNestedProp = (obj, props, value) => {
+    let index = 0
+    let currentValue = obj[props[index]]
+    if (props.length > 1 && typeof currentValue === "object")
+        return getNestedProp(currentValue, props.slice(index + 1))
+
+    obj[props[index]] = value
+    return obj
+}
+
+module.exports = { 
+    getNestedProp, 
+    setNestedProp 
+}
